c17031852881f7440648996cc0771dc1c768d758
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2020-12-17 16:11:30 +0530

    Update __manifest__.py

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -8,7 +8,7 @@
         'sale_management','website','account_accountant'
     ],    
     'data': [
-        #'reports/report_tax_invoice_copy.xml',
+        'reports/report_tax_invoice_copy.xml',
         'reports/report_tax_invoice.xml',
         'reports/reports.xml',
         'views/app_report_view.xml'
