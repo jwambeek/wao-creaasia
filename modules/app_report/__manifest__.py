91fa33ee149dbca93ba9c5ff0a80b916f6dcd9e4
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2020-12-18 16:39:59 +0530

    update

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -11,7 +11,9 @@
         'reports/report_tax_invoice_copy.xml',
         'reports/report_tax_invoice.xml',
         'reports/reports.xml',
-        'views/app_report_view.xml'
+        'views/app_report_view.xml',
+        'reports/custom_header.xml',
+        'reports/custom_footer.xml'
     ],
     'installable': True,
     'application':True,
