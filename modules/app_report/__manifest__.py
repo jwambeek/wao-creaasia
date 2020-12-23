e929f66379c773df48341b656db2003529dc164e
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2020-12-23 13:51:01 +0530

    update

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -9,6 +9,7 @@
     ],    
     'data': [
         #'data/data.xml',
+        'reports/custom_header.xml'
         'reports/report_tax_invoice.xml',
         'reports/reports.xml',
         'views/app_report_testview.xml'
