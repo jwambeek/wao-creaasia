b7f33c966ff8adf946bd9f5fa66ba7e144fb51e6
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2020-12-25 14:19:17 +0530

    Update app_report_test.py

--- modules/app_report/models/app_report_test.py
+++ modules/app_report/models/app_report_test.py
@@ -47,6 +47,7 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     seller_discount = fields.Float(string='Seller Discount')
 
+    @api.multi
     def _prepare_invoice_line(self, qty):
         """
         Prepare the dict of values to create the new invoice line for a sales order line.
