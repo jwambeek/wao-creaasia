5480d3ac1af5c4fcfbb942cfe1937ce8f2ac0cf1
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2020-12-29 17:30:14 +0530

    update

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Tax Reports',
-    'version': '2.7',
+    'version': '2.8',
     'Summary': 'Tax Report Prints',
     'description': 'To print the new report',
     'license': 'LGPL-3',
