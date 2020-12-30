edb8f8e563f9f7d1eeedc3aaee23ee0f9479bbc8
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2020-12-30 17:46:57 +0530

    update report

--- modules/app_report/__manifest__.py
+++ modules/app_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Tax Reports',
-    'version': '2.5',
+    'version': '2.6',
     'Summary': 'Tax Report Prints',
     'description': 'To print the new report',
     'license': 'LGPL-3',
@@ -10,6 +10,7 @@
     'data': [
         'reports/custom_header1.xml',
         'reports/report_tax_invoice1.xml',
+        'reports/custom_header_copy.xml',
         'reports/report_tax_invoice_copy1.xml',
         'reports/reports.xml',
         'views/app_report_testview.xml'
