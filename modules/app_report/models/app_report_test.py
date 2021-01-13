0c4ab2216a3b48a3587a2d9ac622a54fc35c7d73
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-01-13 16:03:27 +0530

    Update app_report_test.py

--- modules/app_report/models/app_report_test.py
+++ modules/app_report/models/app_report_test.py
@@ -98,7 +98,7 @@ class AccountInvoice_Data(models.Model):
 
     channel_order_number = fields.Char(string = 'Channel Order No.',readonly=True, tracking=True)
     number = fields.Char(related='move_id.name', store=True, readonly=True, copy=False)
-    test = fields.Char(related='move_id.total_amount', store=True, readonly=True, copy=False)
+    test = fields.Char(related='move_id.name', store=True, readonly=True, copy=False)
 
     @api.multi
     def action_invoice_draft(self):
