13ec1e3cd5d4540b7454db12ef4d64d0cc2b9a51
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-01-13 13:01:37 +0530

    update

--- modules/app_report/models/app_report_test.py
+++ modules/app_report/models/app_report_test.py
@@ -85,11 +85,82 @@ class SaleOrderLine(models.Model):
         }
         return res
 
+# mapping invoice type to refund type
+TYPE2REFUND = {
+    'out_invoice': 'out_refund',        # Customer Invoice
+    'in_invoice': 'in_refund',          # Vendor Bill
+    'out_refund': 'out_invoice',        # Customer Credit Note
+    'in_refund': 'in_invoice',          # Vendor Credit Note
+}
 
 class AccountInvoice_Data(models.Model):
     _inherit = 'account.invoice'
 
     channel_order_number = fields.Char(string = 'Channel Order No.',readonly=True, tracking=True)
+    number = fields.Char(related='move_id.amount_untaxed', store=True, readonly=True, copy=False)
+
+    @api.model
+    def _prepare_refund(self, invoice, date_invoice=None, date=None, description=None, journal_id=None):
+        """ Prepare the dict of values to create the new credit note from the invoice.
+            This method may be overridden to implement custom
+            credit note generation (making sure to call super() to establish
+            a clean extension chain).
+
+            :param record invoice: invoice as credit note
+            :param string date_invoice: credit note creation date from the wizard
+            :param integer date: force date from the wizard
+            :param string description: description of the credit note from the wizard
+            :param integer journal_id: account.journal from the wizard
+            :return: dict of value to create() the credit note
+        """
+        values = {}
+        for field in self._get_refund_copy_fields():
+            if invoice._fields[field].type == 'many2one':
+                values[field] = invoice[field].id
+            else:
+                values[field] = invoice[field] or False
+
+        values['invoice_line_ids'] = self._refund_cleanup_lines(invoice.invoice_line_ids)
+
+        tax_lines = invoice.tax_line_ids
+        taxes_to_change = {
+            line.tax_id.id: line.tax_id.refund_account_id.id
+            for line in tax_lines.filtered(lambda l: l.tax_id.refund_account_id != l.tax_id.account_id)
+        }
+        cleaned_tax_lines = self._refund_cleanup_lines(tax_lines)
+        values['tax_line_ids'] = self._refund_tax_lines_account_change(cleaned_tax_lines, taxes_to_change)
+
+        if journal_id:
+            journal = self.env['account.journal'].browse(journal_id)
+        elif invoice['type'] == 'in_invoice':
+            journal = self.env['account.journal'].search([('type', '=', 'purchase')], limit=1)
+        else:
+            journal = self.env['account.journal'].search([('type', '=', 'sale')], limit=1)
+        values['journal_id'] = journal.id
+
+        values['type'] = TYPE2REFUND[invoice['type']]
+        values['date_invoice'] = date_invoice or fields.Date.context_today(invoice)
+        values['date_due'] = values['date_invoice']
+        values['state'] = 'draft'
+        values['number'] = False
+        values['origin'] = invoice.number
+        values['refund_invoice_id'] = invoice.id
+        values['reference'] = False
+
+        if values['type'] == 'in_refund':
+            values['payment_term_id'] = invoice.partner_id.property_supplier_payment_term_id.id
+            partner_bank_result = self._get_partner_bank_id(values['company_id'])
+            if partner_bank_result:
+                values['partner_bank_id'] = partner_bank_result.id
+        else:
+            values['payment_term_id'] = invoice.partner_id.property_payment_term_id.id
+
+        if date:
+            values['date'] = date
+        if description:
+            values['name'] = description
+        return values
+
 
     @api.depends('invoice_line_ids.price_unit','invoice_line_ids.quantity')
     def _cal_total_amount(self):
@@ -146,6 +217,7 @@ class AccountInvoice_Data(models.Model):
 
     vat = fields.Float(string = 'Vat', compute = '_cal_total_vat', store = True, digits=(12,4))
 
+    # Function :- Corrected Goods/Service Amount for Credit Note Report
     @api.depends('total_amount','total_baht_excl_VAT')
     def _corrected_service_amount(self):
         for orders in self:
